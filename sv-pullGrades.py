727bbf7033f09729c268baf84142c53be635cc3a
seanyse <67351529+seanyse@users.noreply.github.com>
2022-04-07 23:45:21 -0400

    Update sv-pullGrades.py

--- sv-pullGrades.py
+++ sv-pullGrades.py
@@ -1,75 +1,66 @@
+
 import requests
 from bs4 import BeautifulSoup
-import time
-import json
+import re
 import os 
 
-username = os.environ['USER']
-password = os.environ['PASS']
+def grades():
+    username = os.environ['USER']
+    password = os.environ['PASS']
 
-login_url = " https://parentvue.cobbk12.org/./PXP2_Login_Student.aspx?regenerateSessionId=True"
-gradebook_url = "https://parentvue.cobbk12.org/PXP2_Gradebook.aspx"
+    login_url = " https://parentvue.cobbk12.org/./PXP2_Login_Student.aspx?regenerateSessionId=True"
+    gradebook_url = "https://parentvue.cobbk12.org/PXP2_Gradebook.aspx"
 
-# gather site data
-print("Fetching Site Data . . .")
-r = requests.Session()
-site_data = r.get(login_url)
-soup = BeautifulSoup(site_data.text, "html.parser")
-viewstate = soup.find('input', {"id":"__VIEWSTATE"}).get('value')
-viewstategen = soup.find('input', {"id":"__VIEWSTATEGENERATOR"}).get('value')
-eventvalid = soup.find('input', {"id":"__EVENTVALIDATION"}).get('value')
+    # gather site data
+    print("Fetching Site Data . . .")
+    r = requests.Session()
+    site_data = r.get(login_url)
+    soup = BeautifulSoup(site_data.text, "html.parser")
+    viewstate = soup.find('input', {"id":"__VIEWSTATE"}).get('value')
+    viewstategen = soup.find('input', {"id":"__VIEWSTATEGENERATOR"}).get('value')
+    eventvalid = soup.find('input', {"id":"__EVENTVALIDATION"}).get('value')
 
 
-# create post request payload , login
-login_data = {
-    "__VIEWSTATE": viewstate,
-    "__VIEWSTATEGENERATOR": viewstategen,
-    "__EVENTVALIDATION": eventvalid,
-    "ctl00$MainContent$username": username,
-    "ctl00$MainContent$password": password,
-    "ctl00$MainContent$Submit1": "Login",
-}
+    # create post request payload , login
+    login_data = {
+        "__VIEWSTATE": viewstate,
+        "__VIEWSTATEGENERATOR": viewstategen,
+        "__EVENTVALIDATION": eventvalid,
+        "ctl00$MainContent$username": username,
+        "ctl00$MainContent$password": password,
+        "ctl00$MainContent$Submit1": "Login",
+    }
 
-print("Attemping Login . . .")
-result =  str(r.post(login_url, data = login_data, ))
-resultcheck = result.strip()
+    print("Attemping Login . . .")
+    result =  str(r.post(login_url, data = login_data, ))
+    resultcheck = result.strip()
 
+            
+    if resultcheck == "<Response [200]>":
+        print("Logged In")
         
-if resultcheck == "<Response [200]>":
-    print("Logged In")
+    else:
+        print("Site Error "+resultcheck)
+
+    # pull grades from site
+    print("Fetching Grade Data . . .")
+    grades = r.get(gradebook_url, )
+    site = grades.text
+    soup = BeautifulSoup(grades.text, "html.parser")
+
+    grade = []
+    for i in range(5):
+        try:
+            # add grade to list, remove everything but numbers
+            grade.append(re.sub('\D', '', str(soup.find_all('span', {"class":"mark"})[i])))
+        
+        except IndexError:
+            print("Invalid Login")
+            break
+
+    return grade
+
+
+if __name__ == "__main__":
+    print(grades())
     
-else:
-    print("Site Error "+resultcheck)
-
-# pull grades from site
-print("Fetching Grade Data . . .")
-grades = r.get(gradebook_url, )
-
-site = grades.text
-
-soup = BeautifulSoup(grades.text, "html.parser")
-while True:
-    try:
-        grade1 = soup.find_all('span', {"class":"mark"})[0]
-        grade2 = soup.find_all('span', {"class":"mark"})[1]
-        grade3 = soup.find_all('span', {"class":"mark"})[2]
-        grade4 = soup.find_all('span', {"class":"mark"})[3]
-        grade5 = soup.find_all('span', {"class":"mark"})[4]
-        grade6 = soup.find_all('span', {"class":"mark"})[5]
-        break
-    
-    except IndexError:
-        print("Invalid Login")
-        break
-    
-    except Exception as e:
-        print(e)
-        time.sleep(5)
-        pass
-
-print(grade1)
-print(grade2)
-print(grade3)
-print(grade4)
-print(grade5)
-print(grade6)
