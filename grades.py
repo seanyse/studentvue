3af0e5cc5d3fc3a29c5afd5e9a25ce1cceb493fd
seanyse <67351529+seanyse@users.noreply.github.com>
2022-04-18 17:00:32 -0400

    Update grades.py

--- grades.py
+++ grades.py
@@ -13,53 +13,59 @@ def grades():
 
     # gather site data
     print("Fetching Site Data . . .")
-    r = requests.Session()
-    site_data = r.get(login_url)
-    soup = BeautifulSoup(site_data.text, "html.parser")
-    viewstate = soup.find('input', {"id":"__VIEWSTATE"}).get('value')
-    viewstategen = soup.find('input', {"id":"__VIEWSTATEGENERATOR"}).get('value')
-    eventvalid = soup.find('input', {"id":"__EVENTVALIDATION"}).get('value')
+    try:
+        r = requests.Session()
+        site_data = r.get(login_url)
+        soup = BeautifulSoup(site_data.text, "html.parser")
+        viewstate = soup.find('input', {"id":"__VIEWSTATE"}).get('value')
+        viewstategen = soup.find('input', {"id":"__VIEWSTATEGENERATOR"}).get('value')
+        eventvalid = soup.find('input', {"id":"__EVENTVALIDATION"}).get('value')
 
 
-    # create post request payload , login
-    login_data = {
-        "__VIEWSTATE": viewstate,
-        "__VIEWSTATEGENERATOR": viewstategen,
-        "__EVENTVALIDATION": eventvalid,
-        "ctl00$MainContent$username": username,
-        "ctl00$MainContent$password": password,
-        "ctl00$MainContent$Submit1": "Login",
-    }
+        # create post request payload , login
+        login_data = {
+            "__VIEWSTATE": viewstate,
+            "__VIEWSTATEGENERATOR": viewstategen,
+            "__EVENTVALIDATION": eventvalid,
+            "ctl00$MainContent$username": username,
+            "ctl00$MainContent$password": password,
+            "ctl00$MainContent$Submit1": "Login",
+        }
 
-    print("Attemping Login . . .")
-    result =  str(r.post(login_url, data = login_data, ))
-    resultcheck = result.strip()
+        print("Attemping Login . . .")
+        result =  str(r.post(login_url, data = login_data, ))
+        resultcheck = result.strip()
 
+                
+        if resultcheck == "<Response [200]>":
+            print("Logged In")
             
-    if resultcheck == "<Response [200]>":
-        print("Logged In")
+        else:
+            print("Site Error "+resultcheck)
+            
+        # pull grades from site
+        print("Fetching Grade Data . . .")
+        grades = r.get(gradebook_url, )
+        site = grades.text
+        soup = BeautifulSoup(grades.text, "html.parser")
+
+        grade = []
+        for i in range(12):
+            try:
+                # add grade to list, remove everything but numbers
+                grade.append(re.sub('\D', '', str(soup.find_all('span', {"class":"mark"})[i])))
+                
+            except IndexError:
+                print("Invalid Login")
+                break
+
         
-    else:
-        print("Site Error "+resultcheck)
-
-    # pull grades from site
-    print("Fetching Grade Data . . .")
-    grades = r.get(gradebook_url, )
-    site = grades.text
-    soup = BeautifulSoup(grades.text, "html.parser")
-
-    grade = []
-    for i in range(6):
-        try:
-            # add grade to list, remove everything but numbers
-            grade.append(re.sub('\D', '', str(soup.find_all('span', {"class":"mark"})[i])))
-        
-        except IndexError:
-            print("Invalid Login")
-            break
-    grade = [int(i) for i in grade]
-    return grade
-
+        grade = list(filter(None, grade))
+        grade = [int(i) for i in grade]
+        return grade
+    except Exception as e:
+        print("Error Occured")
+        print(e)
 
 if __name__ == "__main__":
     print(grades())
